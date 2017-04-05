098183394aa4774f3747b4ec588d4b76334c14bc
Alessandra Davila <aledavila@ibm.com>
2017-04-04 21:08:02 -0500

    builds boilerplate

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="title">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'title',
+  data() {
+    return {
+      msg: 'Home Page',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
